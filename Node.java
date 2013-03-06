4a9f6de640fafe81b628f63e0e8b47306c4093fc
leonardnr <leonardnr@s.dcsdk12.org>
2013-03-06 12:46:59 -0700

    wrote node

--- Node.java
+++ Node.java
@@ -6,4 +6,40 @@
  */
 public class Node {
 	
+	private int value;
+	private Node next;
+
+	/**
+	 * Constructors
+	 */
+	public Node(int value, Node next){
+		setValue(value);
+		setNextNode(next);
+	}
+
+	public Node(){
+		this(0, null);
+	}
+
+	public Node(int value){
+		this(value, null);
+	}
+
+	/**
+	 *
+	 * Getters and Setters
+	 * 
+	 */
+	public int getValue(){
+		return this.value;
+	}
+	public void setValue(int value){
+		this.value = value;
+	}
+	public Node getNextNode(){
+		return this.next;
+	}
+	public void setNextNode(Node next){
+		this.next = next;
+	}
 }
